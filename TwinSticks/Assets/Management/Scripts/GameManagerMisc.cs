b75d4766c8f80101f591ff68f70142edb045d2ee
Charles <charlesreverand.01@gmail.com>
2017-09-10 18:33:22 -0400

    v1.8
    Pause with escape and Rewind with control by default.

--- TwinSticks/Assets/Management/Scripts/GameManagerMisc.cs
+++ TwinSticks/Assets/Management/Scripts/GameManagerMisc.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
+
 public class GameManagerMisc : MonoBehaviour {
 
     private static GameObject Manager;
+
+    private bool playBack;
 
     private void Awake()
     {
@@ -15,9 +18,64 @@ public class GameManagerMisc : MonoBehaviour {
         {
             Destroy(gameObject);
         }
-
-
-
     }
 
+    private void Update()
+    {
+        CheckPlayback();
+        CheckPause();
+    }
+
+    void CheckPause()
+    {
+        if (Input.GetButtonDown("Pause") && Time.timeScale != 0)
+        {
+            Pause();
+            
+        }
+        else if (Time.timeScale == 0 && Input.GetButtonDown("Pause"))
+        {
+            Resume();
+        }
+    }
+
+    void Pause()
+    {
+            Time.timeScale = 0;
+            Time.fixedDeltaTime = 0;
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = 0.001f;
+    }
+
+    private void OnApplicationPause()
+    {
+        Pause();
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        Resume();
+    }
+
+
+    public bool PlayBack()
+    {
+        return playBack;
+    }
+
+    private void CheckPlayback()
+    {
+        if (Input.GetButton("Replay"))
+        {
+            playBack = true;
+        }
+        else
+        {
+            playBack = false;
+        }
+    }
 }
---
+++ TwinSticks/Assets/ReplaySystem.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+    private const int bufferFrames = 100;
+    private int maxFrame = -1;
+    private int currentFrame = -1;
+    private KeyFrame[] keyFrames = new KeyFrame[bufferFrames];
+
+    private Rigidbody rb;
+    private GameObject GameManager;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        GameManager = GameObject.Find("GameManager");
+    }
+
+    private void Update()
+    {
+        PlayBackRecord();
+    }
+
+    private void PlayBackRecord()
+    {
+        currentFrame++;
+        if (currentFrame > maxFrame)
+        {
+            maxFrame = currentFrame;
+        }
+        if (currentFrame == bufferFrames)
+        {
+            currentFrame = 0;
+        }
+        if (GameManager.GetComponent<GameManagerMisc>().PlayBack())
+        {
+            PlayBack();
+        }
+        else
+        {
+            Record();
+        }
+    }
+
+    void PlayBack()
+    {
+        rb.isKinematic = true;
+        if (currentFrame == maxFrame)
+        {
+            currentFrame = 0;
+        }
+        int frame = currentFrame % maxFrame;
+
+        frame = Time.frameCount % maxFrame;
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+
+    private void Record()
+    {
+        if (rb.isKinematic)
+        {
+            rb.isKinematic = false;
+        }
+        float time = Time.time;
+        Debug.Log("Writing to frame: " + currentFrame);
+
+        keyFrames[currentFrame] = new KeyFrame(time, transform.position, transform.rotation);
+    }
+}
+
+/// <summary>
+/// A structure for storing time, rotation, and position. Designed to be used for a replay system.
+/// </summary>
+public struct KeyFrame {
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public KeyFrame(float time, Vector3 pos, Quaternion rot)
+    {
+        frameTime = time;
+        position = pos;
+        rotation = rot;
+    }
+}
